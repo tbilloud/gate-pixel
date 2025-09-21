a2268eaa998b482defad1da989aa3f28fbc7b6b2
thomas <tbilloud@icloud.com>
2025-09-21 14:15:21 +0200

    reorder run list and add param for logging

--- examples/run_all.py
+++ examples/run_all.py
@@ -1,11 +1,17 @@
 # Runs all examples at once, used for testing purposes
 
+# Set to True to see logs
+avoid_log = True
+
 import subprocess
 from pathlib import Path
 import os
 
-examples_dir = Path('examples')
-for script in examples_dir.glob('*.py'):
+examples = list(Path('examples').glob('*.py'))
+offline = [e for e in examples if e.name.startswith('offline_')]
+others = [e for e in examples if not e.name.startswith('offline_')]
+ordered = others + offline
+for script in ordered:
     if script.name in ['run_all.py','compare_measure.py']:
         continue
     print('Running',script)
@@ -14,7 +20,7 @@ for script in examples_dir.glob('*.py'):
     env["NAPARI_HEADLESS"] = "1"
     result = subprocess.run(
         ['python', script],
-        capture_output=True,
+        capture_output=avoid_log,
         text=True,
         env=env
     )
