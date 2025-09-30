796e518e53591c4fc2ad92fe347b2069eded62ca
thomas <tbilloud@icloud.com>
2025-09-30 16:44:06 +0200

    simplify example

--- examples/compare_measure.py
+++ examples/compare_measure.py
@@ -1,7 +1,6 @@
 # Show how to compare measurement with simulation
 
 from pathlib import Path
-import numpy as np
 from pandas import read_csv
 from tools.CCevents import local2global, pixelClusters2CCevents, gHits2CCevents
 from tools.utils import charge_speed_mm_ns, create_sensor_object
@@ -25,28 +24,23 @@ single_file, single_name = path_sim / 'gateSingles_blur.root', 'Single_b'
 nhits = 10_000
 hits_meas = read_csv(path_meas, nrows=nhits)
 hits_allp = read_csv(path_sim / 'pixelHits_allpix.csv', nrows=nhits)
-hits_sgls = singles2pixelHits(single_file, spd, thick, single_name, nrows=nhits)
 
 # TOA CUTS
 hits_allp = hits_allp[hits_allp['ToA (ns)'] <= 2.1e10]  # outliers in Gate global time
-hits_sgls = hits_sgls[hits_sgls['ToA (ns)'] <= 2.1e10]  # outliers in Gate global time
 
 # ENERGY CUTS
 emin, emax = 10, source_MeV * 1000
 hits_meas = hits_meas[(hits_meas[ENERGY_keV] > emin) & (hits_meas[ENERGY_keV] < emax)]
 hits_allp = hits_allp[(hits_allp[ENERGY_keV] > emin) & (hits_allp[ENERGY_keV] < emax)]
-hits_sgls = hits_sgls[(hits_sgls[ENERGY_keV] > emin) & (hits_sgls[ENERGY_keV] < emax)]
 
 # BORDER PIXEL CUTS
 cut_edge = 5
 hits_meas = remove_edge_pixels(hits_meas, npix, edge_thickness=cut_edge)
 hits_allp = remove_edge_pixels(hits_allp, npix, edge_thickness=cut_edge)
-hits_sgls = remove_edge_pixels(hits_sgls, npix, edge_thickness=cut_edge)
 
 # ######################### CLUSTERS  ###############################
 clust_meas = pixelHits2pixelClusters(hits_meas, window_ns=100, npix=256)
 clust_allp = pixelHits2pixelClusters(hits_allp, window_ns=100, npix=256)
-clust_sgls = pixelHits2pixelClusters(hits_sgls, window_ns=100, npix=256)
 
 # ENERGY CUTS
 eClstrMax = 260
@@ -57,20 +51,21 @@ clust_allp = clust_allp[clust_allp[ENERGY_keV] < eClstrMax]
 # ######################## CCevents  ################################
 ev_meas = pixelClusters2CCevents(clust_meas, thick=thick, speed=spd, twindow=100)
 ev_allp = pixelClusters2CCevents(clust_allp, thick=thick, speed=spd, twindow=100)
-ev_sgls = pixelClusters2CCevents(clust_sgls, thick=thick, speed=spd, twindow=100)
 plot_energies(min_keV=0, max_keV=260,
-              names=['Measurement', 'Allpix', 'Singles'],
-              hits_list=[hits_meas, hits_allp, hits_sgls],
-              clusters_list=[clust_meas, clust_allp, clust_sgls],
-              CCevents_list=[ev_meas, ev_allp, ev_sgls],
+              names=['Measurement', 'Allpix'],
+              hits_list=[hits_meas, hits_allp],
+              clusters_list=[clust_meas, clust_allp],
+              CCevents_list=[ev_meas, ev_allp],
               ylog=False)
 
 # ############################## RECO  ###############################
 ev_meas = local2global(ev_meas, s.translation, s.rotation, npix, pitch, thick)
 ev_allp = local2global(ev_allp, s.translation, s.rotation, npix, pitch, thick)
-ev_refc = gHits2CCevents(path_sim / 'gateHits.root', source_MeV='Cd111[245.390]_245.390', entry_stop=nhits * 50)
+ev_refc = gHits2CCevents(path_sim / 'gateHits.root', source_MeV='Cd111[245.390]_245.390', entry_stop=nhits * 2)
 reco_params = {'method': 'torch',
-               'vpitch': 10, 'vsize': (32, 32, 32), 'cone_width': 0.05,
+               'vpitch': 1,
+               'vsize': tuple(int(x * 4) for x in [s.size[0], s.size[1], abs(sensor_pos[2])]),
+               'cone_width': 0.002,
                'energies_MeV': [source_MeV], 'tol_MeV': 0.03}
 v_meas = reconstruct(ev_meas, **reco_params)
 v_allp = reconstruct(ev_allp, **reco_params)
