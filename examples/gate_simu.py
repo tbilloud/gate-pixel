168503ac14708a9efcbeef651d489bd71b178647
tbilloud <tbilloud@gmail.com>
2025-10-24 15:43:38 +0200

    remove pyvista from requirements

--- examples/gate_simu.py
+++ examples/gate_simu.py
@@ -12,7 +12,7 @@ um, mm, keV, Bq, ms = g4_units.um, g4_units.mm, g4_units.keV, g4_units.Bq, g4_un
 def gate_simu(sensor_material="G4_CADMIUM_TELLURIDE"):
     sim, sim.output_dir = Simulation(), Path("output")
     sim.random_seed = 1
-    sim.visu = False
+    sim.visu = False # if needed, install pyvista
     sim.verbose_level = 'DEBUG'  # DEBUG for data preview, INFO for algo timing only
 
     # ===========================
